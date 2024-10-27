8630080f9175ac8eea321c3482aabbb94a678673
manaheel jamil <manaheeljamil20@gmail.com>
2024-10-27 12:36:07 +0500

    array sort

--- userValue.js
+++
@@ -1,8 +0,0 @@
-const user = prompt("your array length?")
-const array = []
-for(var i =0 ;i < user;i++){
-    const input = prompt("write your numbers")
-    array.push(input)
-}
-
-console.log(array)
